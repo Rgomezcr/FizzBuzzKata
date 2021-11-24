33456333204f71d6975ac579a37e9d5983828e13
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-24 14:32:01 +0100

    feat: Return three as string

--- FizzBuzzKata/FizzBuzzKata.cs
+++ FizzBuzzKata/FizzBuzzKata.cs
@@ -14,6 +14,8 @@ namespace FizzBuzzKata
                 return "1";
             if (number == 2)
                 return "2";
+            if (number == 3)
+                return "Fizz";
             return "4";
         }
     }
