7a2c599820d9695ef779847cdb8f3ef0998cb79c
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-24 11:10:29 +0100

    Add project files.

---
+++ FizzBuzzKata.Tests/FizzBuzzKataShould.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FizzBuzzKata.Tests
+{
+    public class FizzBuzzKataShould
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
