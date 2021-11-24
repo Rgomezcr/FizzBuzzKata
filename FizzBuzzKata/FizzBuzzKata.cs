d2b2f5f042e011537c5f20fe09f29b75a8e9c27b
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-24 14:03:11 +0100

    feat: Return four as string

--- FizzBuzzKata/FizzBuzzKata.cs
+++ FizzBuzzKata/FizzBuzzKata.cs
@@ -12,7 +12,9 @@ namespace FizzBuzzKata
         {
             if (number == 1)
                 return "1";
-            return "2";
+            if (number == 2)
+                return "2";
+            return "4";
         }
     }
 }
