41529d3f3e4277e0f4c9148e94d9220b0f552a5a
Raúl Gómez <rgomez@voxelgroup.net>
2021-11-24 22:19:43 +0100

    refactor: generalize return number as string solution

--- FizzBuzzKata/FizzBuzzKata.cs
+++ FizzBuzzKata/FizzBuzzKata.cs
@@ -10,19 +10,14 @@ namespace FizzBuzzKata
     {
         public string ReturnNumberAsString(int number)
         {
-            if (number == 1)
-                return "1";
-            if (number == 2)
-                return "2";
             if (number == 3)
                 return "Fizz";
             if (number == 5)
                 return "Buzz";
             if (number == 6)
                 return "Fizz";
-            if (number == 7)
-                return "7";
-            return "4";
+
+            return Convert.ToString(number);
         }
     }
 }
